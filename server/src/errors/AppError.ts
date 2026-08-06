6c72b54c16c386292078bd2969c34d23a1a013f3
Kevin Marlin <kevin.marlin@gmail.com>
2026-08-06 14:00:39 +0200

    feat(error): add extends of Error for handler a personal error format

---
+++ server/src/errors/AppError.ts
@@ -0,0 +1,13 @@
+export class AppError extends Error {
+
+    statusCode: number;
+    code: string;
+    
+    constructor(message: string, statusCode: number, code: string ) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.code = code;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
